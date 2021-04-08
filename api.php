c28cdf36f4bb7422b7458e5fd0abb3de66326f9c
sflittrell <sflittrell@gmail.com>
2021-04-08 15:23:24 -0400

    code from end of video tutorial

--- api.php
+++ api.php
@@ -1,18 +1,16 @@
-<?p
+<?php
 
 $servername = "localhost";
-$username = "username";
+$username = "root";
 $password = "My#password";
-$dbname = "sql_heroes"
+$dbname = "sql_heroes";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 
 $route = $_GET['route'];
 
@@ -20,34 +18,105 @@ switch ($route) {
   case "getAllHeroes":
     $myData = getAllHeroes($conn);
     break;
-  case "createBattle":
-    $myData = createBattle($conn, 2, 4, 4);
-    break;
   case "getHeroByID":
-    //
-    $id = $_GET["hero_id"];
-    $myData = getHeroByID($conn, $id);
+    $id = $_GET['hero_id'];
+    $myData = getHeroById($conn, $id);
+    break;
+  case "getAllAbilities":
+    $myData = getAllAbilities($conn);
     break;
   default:
-   $myData = json_encode([]);
+    $myData = json_encode([]);
 }
 
 echo $myData;
 
+
 $conn->close();
 
 function getAllHeroes($conn){
-  $data=array();
+  $data = array();
   
-  $sql = "SELECT * FROM heroes";
+    $sql = "SELECT * FROM heroes";
   $result = $conn->query($sql);
-  
-   // blank array
+
   if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-//          unset($row['nombre']);
-         array_push($data,$row); // push rows to array $myData
-      }
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      array_push($data, $row);
+    }
   } 
+  
+  return json_encode($data);
+}
+
+
+function getHeroByID($conn, $heroID) {
+  $data = array();
+  
+    $sql = "SELECT * FROM heroes WHERE id = " . $heroID;
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      array_push($data, $row);
+    }
+  } else {
+    $data = "0 results";
+  }
+  
+  return json_encode($data);
+}
+
+function getAllAbilities($conn){
+  $data = array();
+  
+    $sql = "SELECT * FROM abilities";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      array_push($data, $row);
+    }
+  } 
+  
+  return json_encode($data);
+}
+
+function getAllAbilities($conn){
+  $data = array();
+  
+    $sql = "SELECT * FROM abilities";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      array_push($data, $row);
+    }
+  } 
+  
+  return json_encode($data);
+}
+
+// function getAllAbilities($conn){
+//   $data = array();
+  
+//     $sql = "SELECT * FROM abilities";
+//   $result = $conn->query($sql);
+
+//   if ($result->num_rows > 0) {
+//     // output data of each row
+//     while($row = $result->fetch_assoc()) {
+//       array_push($data, $row);
+//     }
+//   } 
+  
+//   return json_encode($data);
+// }
+
+
 
 ?>
--- index.php
+++ index.php
@@ -8,17 +8,31 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>SQL Heroes</title>
   </head>
   <body>
-    <h1>SQL Heros</h1>
-    <h3>Menu</h3>
-    
-    <ul>
-         <li><a href="/api.php?" target="_blank">Test</a></li>
-    </ul>
+    <h1>SQL Heroes</h1>
 
     <!-- Optional JavaScript; choose one of the two! -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-lg-12">
+          <h1>
+            Menu
+          </h1>
+          <ul>
+            <li><a href="/api.php?route=getAllHeroes" target="_blank">Get All Heroes</a><br>
+            <em>show all heros</em></li>
+            <li><a href="/api.php?route=getHeroByID&hero_id=5" target="_blank">Get Hero By ID</a><br>
+            <em>change the id in the address bar to choose the hero</em></li>
+            <li><a href="/api.php?route=getAllAbilities" target="_blank">Get All Abilities</a><br>
+            <em>show all hero abilities</em></li>
+          </ul>
+          
+        </div>
+      </div>
+    </div>
+    
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
